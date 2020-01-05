5725fb24913834e49319f00879d7ee241d42b667
Firuz Afzalov <afzalov98@gmail.com>
2020-01-05 23:27:20 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -50,7 +50,7 @@ def naming_system
       
     sub_category:{
       label: "Order",
-    sub_category {
+    
     label: "Family",
     sub_category: {
       label: "Genus",
@@ -63,6 +63,5 @@ def naming_system
       }
     }
   }
-   }
 
 end
